12ce7990fff68c381e0e4af2c10661d7f5a4c9e9
keitakn <keita.koga.work@gmail.com>
2020-01-08 19:14:42 +0900

    :tada: #1 シンプルなHTTPサーバーを動かす最低限のコードを追加

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type String string
+
+func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, s)
+}
+
+func main() {
+	msg := "Hello World Client🐱"
+	http.Handle("/", String(msg))
+	http.ListenAndServe(":9999", nil)
+}

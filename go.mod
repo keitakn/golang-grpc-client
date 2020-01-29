3ad5dd63ff7f9e858c8b3b55edd164c8d1cdeba0
keitakn <keita.koga.work@gmail.com>
2020-01-29 14:15:42 +0900

    :sparkles: #3 golang-grpc-serverに通信する最低限のコードを追加

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/keitakn/golang-grpc-client
 
 go 1.13
+
+require (
+	github.com/keitakn/golang-grpc-server v0.0.0-20200110010247-5df29d624f40
+	google.golang.org/grpc v1.27.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	pb "github.com/keitakn/golang-grpc-server/pb"
+	"google.golang.org/grpc"
+	"log"
 	"net/http"
+	"time"
 )
 
 type String string
@@ -11,8 +16,27 @@ func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, s)
 }
 
+// TODO 後で構造化する
 func main() {
-	msg := "Hello World Client🐱"
+	// TODO gRPCサーバーの情報は環境変数等にする
+	// TODO gRPCサーバーの生成処理は分離する
+	connection, err := grpc.Dial("grpc-server:9998", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln("did not connect: %s", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewCatClient(connection)
+
+	context, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := client.FindCuteCat(context, &pb.FindCuteCatMessage{CatId: "mop"})
+	if err != nil {
+		log.Println(err)
+	}
+
+	msg := response.GetName() + " is " + response.GetKind() + " 🐱"
 	http.Handle("/", String(msg))
 	http.ListenAndServe(":9999", nil)
 }
